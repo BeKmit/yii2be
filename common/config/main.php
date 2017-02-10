f255f3ef7eb5f7ca4a50acf66879c497f9288a1d
Pichasit Nutthanapong <pichasit.n@cfg.co.th>
2017-02-10 16:06:47 +0700

    Fix bug displayTimezone in kartik\detail\DetailView data wrong at page view

--- common/config/main.php
+++ common/config/main.php
@@ -18,18 +18,33 @@ return [
             # Disable r= routes
             'enablePrettyUrl' => true,
         ],
+
+        'formatter' => [
+            'defaultTimeZone' => 'Asia/Bangkok',
+            'dateFormat' => 'php: d-m-Y',
+            'timeFormat' => 'php: H:i',
+            'datetimeFormat' => 'php: d-m-Y H:i',
+            'currencyCode' => '฿',
+            'decimalSeparator' => ',',
+            'thousandSeparator' => ' ',
+        ],
     ],
 
     'modules' => [
         # warrence/yii2-kartikgii
+        // Fix bug displayTimezone in kartik\detail\DetailView data wrong at page view
+        // You must add Config->components = ['formatter'=>['defaultTimeZone' => 'Asia/Bangkok']]
         'datecontrol' =>  [
             'class' => 'kartik\datecontrol\Module',
+
+            // set your display timezone
+            'displayTimezone' => 'Asia/Bangkok',
 
             // format settings for displaying each date attribute
             'displaySettings' => [
                 'date' => 'php: d-m-Y',
-                'time' => 'php: H:i:s',
-                'datetime' => 'php: d-m-Y H:i:s',
+                'time' => 'php: H:i',
+                'datetime' => 'php: d-m-Y H:i',
             ],
 
             // format settings for saving each date attribute
@@ -41,6 +56,6 @@ return [
 
             // automatically use kartik\widgets for each of the above formats
             'autoWidget' => true,
-        ]
+        ],
     ],
 ];
